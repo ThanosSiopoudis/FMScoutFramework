07229305bf8b876763a6a181f4b1f88b8d429329
ThanosSiopoudis <siopoudis@me.com>
2015-07-10 15:29:02 +0100

    Add Teams and RivalNations to Nations

--- Entities/Ingame/Nation.cs
+++ Entities/Ingame/Nation.cs
@@ -32,6 +32,40 @@ namespace FMScoutFramework.Core.Entities.InGame
 			}
 		}
 
+        public Team[] Teams
+        {
+            get
+            {
+                int teamCount = ProcessManager.ReadArrayLength(MemoryAddress + NationOffsets.Teams);
+                Team[] result = new Team[teamCount];
+
+                for (int i = 0; i < teamCount; i++)
+                {
+                    int teamAddress = PropertyInvoker.Get<Int32>(NationOffsets.Teams, OriginalBytes, MemoryAddress, DatabaseMode);
+                    result[i] = PropertyInvoker.GetPointer<Team>(0x0, OriginalBytes, (teamAddress + (i * 4)), DatabaseMode, Version);
+                }
+
+                return result;
+            }
+        }
+
+        public RivalNation[] RivalNations
+        {
+            get
+            {
+                int nationCount = ProcessManager.ReadArrayLength(MemoryAddress + NationOffsets.RivalNations, 0xC);
+                RivalNation[] result = new RivalNation[nationCount];
+
+                for (int i = 0; i < nationCount; i++)
+                {
+                    int nationAddress = PropertyInvoker.Get<Int32>(NationOffsets.RivalNations, OriginalBytes, MemoryAddress, DatabaseMode);
+                    result[i] = new RivalNation((nationAddress + (i * 0xC)), Version);
+                }
+
+                return result;
+            }
+        }
+
 		public override string ToString ()
 		{
 			return Name;
