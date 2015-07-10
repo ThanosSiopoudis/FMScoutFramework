cd8b8922bb7c64c03a15f1a3e998056074271356
ThanosSiopoudis <siopoudis@me.com>
2015-07-10 15:28:45 +0100

    Add a new entity type "RivalNations"

---
+++ Defines/Offsets/Sub-Entities/RivalNationOffsets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FMScoutFramework.Core.Entities.GameVersions;
+
+namespace FMScoutFramework.Core.Offsets
+{
+    public sealed class RivalNationOffsets
+    {
+        public IVersion Version;
+        public RivalNationOffsets(IVersion version)
+        {
+            this.Version = version;
+        }
+
+        public const short NationAddress    = 0x0;
+        public const short Level = 0x8;
+    }
+}
---
+++ Entities/Ingame/Sub-Entities/RivalNation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FMScoutFramework.Core.Entities.GameVersions;
+using FMScoutFramework.Core.Managers;
+using FMScoutFramework.Core.Offsets;
+using FMScoutFramework.Core.Attributes;
+
+namespace FMScoutFramework.Core.Entities.InGame
+{
+    public class RivalNation : BaseObject
+    {
+        public RivalNationOffsets RivalNationOffsets;
+        public RivalNation (int memoryAddress, IVersion version)
+            : base(memoryAddress, version) 
+        {
+                this.RivalNationOffsets = new RivalNationOffsets(version);
+        }
+        public RivalNation(int memoryAddress, ArraySegment<byte>originalBytes, IVersion version)
+            : base(memoryAddress, originalBytes, version)
+        {
+            this.RivalNationOffsets = new RivalNationOffsets(version);
+        }
+
+        private int RivalNationAddress
+        {
+            get
+            {
+                return PropertyInvoker.Get<Int32>(RivalNationOffsets.NationAddress, OriginalBytes, MemoryAddress, DatabaseMode);
+            }
+        }
+
+        public Nation Nation
+        {
+            get
+            {
+                return PropertyInvoker.GetPointer<Nation>(RivalNationOffsets.NationAddress, OriginalBytes, MemoryAddress, DatabaseMode, Version);
+            }
+        }
+
+        public Byte Level
+        {
+            get
+            {
+                return PropertyInvoker.Get<Byte>(RivalNationOffsets.Level, OriginalBytes, MemoryAddress, DatabaseMode);
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Nation.Name;
+        }
+    }
+}
